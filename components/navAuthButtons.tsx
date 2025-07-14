eac0ed4f3373ed337c6fc3a50b91f399a3e77875
Jean Paul Fernandez <jpxoi@icloud.com>
2025-07-14 13:47:19 -0500

    Add NavAuthButtons and auth button components for improved user authentication
    - Introduced NavAuthButtons component to manage user authentication states with Clerk.
    - Created SignInButton, SignUpButton, and SignOutButton components for streamlined authentication actions.
    - Enhanced user experience by integrating loading states and conditional rendering based on user sign-in status.

---
+++ components/navAuthButtons.tsx
@@ -0,0 +1,29 @@
+import { ClerkLoading, ClerkLoaded, SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton, SignUpButton } from "./auth/authButtons";
+import CustomUserButton from "./auth/customUserButton";
+
+export function NavAuthButtons() {
+  return (
+    <>
+      <ClerkLoading>
+        <div className="flex items-center gap-2">
+          <SignInButton variant="outline" size="sm" />
+          <SignUpButton size="sm" />
+        </div>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <div className="h-7 w-7">
+            <CustomUserButton />
+          </div>
+        </SignedIn>
+        <SignedOut>
+          <div className="flex items-center gap-2">
+            <SignInButton variant="outline" size="sm" />
+            <SignUpButton size="sm" />
+          </div>
+        </SignedOut>
+      </ClerkLoaded>
+    </>
+  );
+}
